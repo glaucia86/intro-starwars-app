83b9d6f50190f78e8fb3a694d8c7c3177c7257e3
glaucia86 <glaucia_lemos86@hotmail.com>
2019-12-20 20:07:08 -0300

    desenvolvimento do express.js para permitir exec de files estáticos.

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+/**
+ * Arquivo: app.js
+ * Descrição: Arquivo responsável por executar a aplicação e levantar o servidor
+ * local.
+ * Data: 20/12/2019
+ * Author: Glaucia Lemos
+ */
+
 const express = require('express');
+
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('App - Intro Star Wars')
+  res.send('App - Intro Star Wars');
 });
 
 app.listen(3000, () => console.log('Aplicação executando na porta 3000!'));
