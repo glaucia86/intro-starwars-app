c9cda2b7a554f8678148852eabde610f39bd386b
glaucia86 <glaucia_lemos86@hotmail.com>
2019-12-20 19:15:15 -0300

    Initial commit.

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('App - Intro Star Wars')
+});
+
+app.listen(3000, () => console.log('Aplicação executando na porta 3000!'));
